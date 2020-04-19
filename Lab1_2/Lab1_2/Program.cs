e4a9b36dacc8e5749a67ad76d7f84f75ec90b91b
Nastya_Levitova <nastya.levitova99@gmail.com>
2020-04-19 17:07:25 +0300

    Вывод только четных символов

---
+++ Lab1_2/Lab1_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вывод только четных символов
+            Console.WriteLine("Введите цифры");
+            string str = Console.ReadLine();
+            string[] s = str.Split(' ');
+            for (int i = 0; i < s.Length; i++)
+                if (int.Parse(s[i]) % 2 == 0)
+                    Console.Write("{0} ", s[i]);
+
+        }
+    }
+}
