ebe05450abb55d722823f3ecf0bf1f9216ba011c
Nastya_Levitova <nastya.levitova99@gmail.com>
2020-04-19 17:07:03 +0300

    Сложение всех чётных чисел в массиве

---
+++ Lab1_1/Lab1_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Сложение всех чётных чисел в массиве 
+            int s = 0;
+            int[] x = new int[10];
+            x[0] = 1;
+            x[1] = 2;
+            x[2] = 3;
+            x[3] = 4;
+            x[4] = 5;
+            x[5] = 6;
+            x[6] = 7;
+            x[7] = 8;
+            x[8] = 9;
+            x[9] = 10;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (x[i] % 2 == 0)
+                {
+                    s = s + x[i];
+                }
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
